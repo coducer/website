e74c9de1a5ead5c18a4843bd6f761fec468c2ce9
gobu <gobu@gobu-Vostro-1550.(none)>
2012-12-28 12:10:27 +0530

    coducer website

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+  def career
+  end
+  def apply
+  end 
+  def details
+  end
+  def product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Website::Application.routes.draw do
-  root :to => 'home#index'
+  root :to => "home#index"
+  get "/careers" => "home#careers"
+  get "/careers/apply" => "home#apply"
+  get "/careers/details" => "home#details"
+  get "/product" => "home#product"
 end
